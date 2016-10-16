ff350a45d995c96246223a8c3edd261e23d25030
Denis Bakhvalov <dendibakh@gmail.com>
2016-10-16 21:48:52 +0200

    added-more-tests-on-rvlue-refs

---
+++ MoveSemantics/MoveBindingsTest.cpp
@@ -0,0 +1,322 @@
+/*
+ * MoveBindingsTest.cpp
+ *
+ *  Created on: Oct 14, 2016
+ *      Author: bakhvalo
+ */
+
+#include "gtest/gtest.h"
+#include <type_traits>
+#include <iostream>
+
+using namespace ::testing;
+
+int foo(int &&x)
+{
+	return x;
+}
+
+TEST(MoveBindingsTest, binding_references)
+{
+	foo(5);
+
+	int x = 5;
+	// foo(x); compiler error - can't bind int to int&&.
+
+	foo(std::move(x));
+
+	int& y = x;
+	// foo(y); compiler error - can't bind int to int&&.
+
+	foo(std::move(y));
+}
+
+namespace
+{
+	class testMoves
+	{
+	public:
+		testMoves() { std::cout << "testMoves()" << '\n'; }
+		testMoves(const testMoves& o)				{ std::cout << "testMoves(&)" << '\n';  x = 1; }
+		testMoves& operator=(const testMoves& o) 	{ std::cout << "operator=(&)" << '\n';  x = 2; return *this; }
+		testMoves(testMoves&& o)					{ std::cout << "testMoves(&&)" << '\n'; x = 3; o.x = 3;}
+		testMoves& operator=(testMoves&& o) 		{ std::cout << "operator=(&&)" << '\n'; x = 4; o.x = 4; return *this; }
+		~testMoves() {}
+		int x {0};
+	};
+}
+
+template <class T>
+class TD;
+
+// Returning an rvalue object by value.
+// obj1 is a copy of obj. no magic here.
+// Temporary is always created.
+
+testMoves bar (testMoves&& obj)
+{
+	return obj;
+}
+
+TEST(MoveBindingsTest, taking_returned_object_by_value)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	testMoves obj1 = bar(std::move(obj));
+	ASSERT_EQ(0, obj.x);
+	ASSERT_EQ(1, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_returned_object_by_lvalue_ref_not_compiles)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+//	testMoves& obj2 = bar(std::move(obj));
+//	ASSERT_EQ(0, obj.x);
+//	ASSERT_EQ(1, obj2.x);
+}
+
+TEST(MoveBindingsTest, taking_returned_object_by_const_ref)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	const testMoves& obj2 = bar(std::move(obj));
+	ASSERT_EQ(0, obj.x);
+	ASSERT_EQ(1, obj2.x);
+	static_assert( std::is_same< decltype(obj2), const testMoves&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_object_by_rvalue)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	testMoves&& obj2 = bar(std::move(obj)); // is it an undefined behavior?
+	ASSERT_EQ(0, obj.x);
+	ASSERT_EQ(1, obj2.x);
+	static_assert( std::is_same< decltype(obj2), testMoves&&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_object_by_const_rvalue)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	const testMoves&& obj2 = bar(std::move(obj)); // is it an undefined behavior?
+	ASSERT_EQ(0, obj.x);
+	ASSERT_EQ(1, obj2.x);
+	static_assert( std::is_same< decltype(obj2), const testMoves&&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_object_deduced_type_is_value)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	auto obj1 = bar(std::move(obj));
+	ASSERT_EQ(0, obj.x);
+	ASSERT_EQ(1, obj1.x);
+	static_assert( std::is_same< decltype(obj1), testMoves>::value, "types are not the same" );
+}
+
+// Returning an rvalue object by rvalue.
+// Here obj1 will be move-constructed from obj.
+
+testMoves barMove (testMoves&& obj)
+{
+	return std::move(obj);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_by_value)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	testMoves obj1 = barMove(std::move(obj));
+	ASSERT_EQ(3, obj.x);
+	ASSERT_EQ(3, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_by_ref_not_compiles)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+//	testMoves& obj1 = barMove(std::move(obj));
+//	ASSERT_EQ(3, obj.x);
+//	ASSERT_EQ(3, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_by_const_ref)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	const testMoves& obj1 = barMove(std::move(obj));
+	ASSERT_EQ(3, obj.x);
+	ASSERT_EQ(3, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_by_rvalue_ref)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	testMoves&& obj1 = barMove(std::move(obj));
+	ASSERT_EQ(3, obj.x);
+	ASSERT_EQ(3, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_by_const_rvalue_ref)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	const testMoves&& obj1 = barMove(std::move(obj));
+	ASSERT_EQ(3, obj.x);
+	ASSERT_EQ(3, obj1.x);
+}
+
+TEST(MoveBindingsTest, taking_moved_object_deduced_type_is_value)
+{
+	testMoves obj;
+	ASSERT_EQ(0, obj.x);
+
+	auto obj1 = barMove(std::move(obj));
+	ASSERT_EQ(3, obj.x);
+	ASSERT_EQ(3, obj1.x);
+	static_assert( std::is_same< decltype(obj1), testMoves>::value, "types are not the same" );
+}
+
+// here is 100% RVO will take place.
+
+testMoves barReturnLocal ()
+{
+	testMoves obj;
+	EXPECT_EQ(0, obj.x);
+	return obj;
+}
+
+TEST(MoveBindingsTest, taking_returned_local_object_by_value)
+{
+	testMoves obj = barReturnLocal();
+	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_local_object_by_ref_not_compiles)
+{
+//	testMoves& obj = barReturnLocal();
+//	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+//	static_assert( std::is_same< decltype(obj), testMoves&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_local_object_by_const_ref)
+{
+	const testMoves& obj = barReturnLocal();
+	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), const testMoves&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_local_object_by_rvalue_ref)
+{
+	testMoves&& obj = barReturnLocal();
+	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves&&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_returned_local_object_by_const_rvalue_ref)
+{
+	const testMoves&& obj = barReturnLocal();
+	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), const testMoves&&>::value, "types are not the same" );
+}
+
+// here RVO won't take place.
+// First, local object will be created and them moved into the returned value.
+// So, one additional move.
+
+testMoves barMoveLocal ()
+{
+	testMoves obj;
+	EXPECT_EQ(0, obj.x);
+	return std::move(obj);
+}
+
+TEST(MoveBindingsTest, taking_moved_local_object_by_value)
+{
+	testMoves obj = barMoveLocal();
+	ASSERT_EQ(3, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_moved_local_object_by_ref_not_compiles)
+{
+//	testMoves& obj = barMoveLocal();
+//	ASSERT_EQ(0, obj.x); // here it is 0, because RVO took place.
+//	static_assert( std::is_same< decltype(obj), testMoves&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_moved_local_object_by_const_ref)
+{
+	const testMoves& obj = barMoveLocal();
+	ASSERT_EQ(3, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), const testMoves&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_moved_local_object_by_rvalue_ref)
+{
+	testMoves&& obj = barMoveLocal();
+	ASSERT_EQ(3, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves&&>::value, "types are not the same" );
+}
+
+TEST(MoveBindingsTest, taking_moved_local_object_by_const_rvalue_ref)
+{
+	const testMoves&& obj = barMoveLocal();
+	ASSERT_EQ(3, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), const testMoves&&>::value, "types are not the same" );
+}
+
+// Attempt to break RVO.
+// local object COPIED into returned object.
+
+testMoves barMoveLocalCond1 (bool cond)
+{
+	testMoves obj1;
+	testMoves obj2;
+	EXPECT_EQ(0, obj1.x);
+	EXPECT_EQ(0, obj2.x);
+	return cond ? obj1 : obj2;
+}
+
+TEST(MoveBindingsTest, no_rvo_taking_moved_local_object_by_value)
+{
+	testMoves obj = barMoveLocalCond1(false);
+	ASSERT_EQ(1, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves>::value, "types are not the same" );
+}
+
+// Attempt to break RVO.
+// local object MOVED into returned object.
+
+testMoves barMoveLocalCond2 (bool cond)
+{
+	testMoves obj1;
+	testMoves obj2;
+	EXPECT_EQ(0, obj1.x);
+	EXPECT_EQ(0, obj2.x);
+	if (cond)
+		return obj1;
+	return obj2;
+}
+
+TEST(MoveBindingsTest, rvo_taking_moved_local_object_by_value)
+{
+	testMoves obj = barMoveLocalCond2(false);
+	ASSERT_EQ(3, obj.x); // here it is 0, because RVO took place.
+	static_assert( std::is_same< decltype(obj), testMoves>::value, "types are not the same" );
+}
--- MoveSemantics/move.cpp
+++ MoveSemantics/move.cpp
@@ -7,6 +7,7 @@
 
 #include "gtest/gtest.h"
 #include <type_traits>
+#include <iostream>
 
 template<typename T>
 decltype(auto) myMove(T&& param)
@@ -31,11 +32,11 @@ namespace
 	class testMoves
 	{
 	public:
-		testMoves() {}
-		testMoves(const testMoves&)				{ x = 1;}
-		testMoves& operator=(const testMoves&) 	{ x = 2; return *this; }
-		testMoves(testMoves&&)					{ x = 3;}
-		testMoves& operator=(testMoves&&) 		{ x = 4; return *this; }
+		testMoves() { std::cout << "testMoves()" << '\n'; }
+		testMoves(const testMoves& o)				{ std::cout << "testMoves(&)" << '\n';  x = 1; }
+		testMoves& operator=(const testMoves& o) 	{ std::cout << "operator=(&)" << '\n';  x = 2; return *this; }
+		testMoves(testMoves&& o)					{ std::cout << "testMoves(&&)" << '\n'; x = 3; o.x = 3;}
+		testMoves& operator=(testMoves&& o) 		{ std::cout << "operator=(&&)" << '\n'; x = 4; o.x = 4; return *this; }
 		~testMoves() {}
 		int x {0};
 	};
@@ -51,10 +52,61 @@ namespace
 	}
 }
 
+template <class T>
+class TD;
+
 TEST(MoveUnitTest, move_cant_produce_rvalue_from_const_object)
 {
 	const testMoves a;
-	fooConstParam(std::move(a)); // const object cannot be moved out.
+	static_assert( std::is_same< decltype(std::move(a)), const testMoves&&>::value, "types are not the same" );
+
+	fooConstParam(std::move(a));
+	// const object cannot be moved out.
+	// because it will not match the function signatire (testMoves&&, not const testMoves&&).
+
 	testMoves b;
 	fooParam(std::move(b));
+}
+
+namespace
+{
+	class A
+	{
+	public:
+		A() {}
+		A(const A& rhs) = delete;
+		A& operator= (const A& rhs) = delete;
+		A(A&& rhs) {}
+		A& operator=(A&& rhs) {return *this;}
+	};
+
+	void bar(A&& b)
+	{
+		static_assert( std::is_same< decltype(b), A&&>::value, "types are not the same" );
+	}
+
+	void foo(A&& a)
+	{
+		static_assert( std::is_same< decltype(a), A&&>::value, "types are not the same" );
+
+		//bar(a);
+		bar(std::move(a));
+
+		//A b(a); // not compiles because it calls copy-constructor
+		A b(std::move(a)); // calls move-constructor
+	}
+}
+
+TEST(MoveUnitTest, we_need_to_moved_rvalue_ref_if_it_is_function_parameter)
+{
+	/*
+	In some cases, you’ll want to use the object bound to an rvalue reference or a universal reference more than once
+	in a single function, and you’ll want to make sure that it’s not moved from until
+	you’re otherwise done with it. In that case, you’ll want to apply std::move (for rvalue
+	references) or std::forward (for universal references) to only the final use of the
+	reference
+	*/
+	A a;
+	//foo(a);
+	foo(std::move(a));
 }
